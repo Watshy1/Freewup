b7fa0fd944f73bd9730d4205c4e6c29c753fdc77
Willi2003 <93141936+Willi2003@users.noreply.github.com>
2022-05-09 09:51:29 +0200

    correct few bugs and add JS for register page

--- public/assets/JavaScript/register_login/register.js
+++ public/assets/JavaScript/register_login/register.js
@@ -1,5 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    
+    let input_password = document.querySelector('#input_password')
+    let input_password_verif = document.querySelector('#input_password_verif')
+    let hide_show_password = document.querySelector('.hide_show_password')
+    let hide_show_password_verif = document.querySelector('.hide_show_password_verif')
+
+    hide_show_password.addEventListener('click', () => {
+        if (input_password.type == "password") {
+            input_password.type = "text"
+        } else {
+            input_password.type = "password"
+        }
+    })
+
+    hide_show_password_verif.addEventListener('click', () => {
+        if (input_password_verif.type == "password") {
+            input_password_verif.type = "text"
+        } else {
+            input_password_verif.type = "password"
+        }
+    })
 
 })
