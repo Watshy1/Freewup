b7f61aba3e861a2f3a5e51b1f26019917bc5c17a
Willi2003 <93141936+Willi2003@users.noreply.github.com>
2022-11-09 10:33:22 +0100

    last commit

--- config/Model.php
+++ config/Model.php
@@ -10,7 +10,7 @@ class Model
 
     public function __construct()
     {
-        $this->pdo = new PDO('mysql:host=;dbname=gajc9642_freewup;charset=UTF8', "", "", [
+        $this->pdo = new PDO('mysql:host=phpmyadmin.student-sup.info;dbname=gajc9642_freewup;charset=UTF8', "gajc9642_william-q", "3C&U2EaQLd!Y(vq5sz", [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::MYSQL_ATTR_FOUND_ROWS => true
             ]
